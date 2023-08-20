71b42bd94faacdec6187c2784d5cc2a82473c82c
Michal <michal.naszko@gmail.com>
2023-08-20 09:12:13 +0200

    Deleting files

--- server.go
+++ server.go
@@ -3,11 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+	"scraper/scraper"
+	"time"
+
+	"example.com/cyclic"
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("public/")))
+	cyclic.Schedule(scraper.QueryOlx, 10*time.Hour.Minutes())
 
+	http.Handle("/", http.FileServer(http.Dir("public/")))
 	port := ":9999"
 	log.Fatal(http.ListenAndServe(port, nil))
 }
